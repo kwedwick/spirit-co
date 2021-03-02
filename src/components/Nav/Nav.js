7b2b41e1ba2030586a808550af6d3ecb796520e1
KWedwick <64446581+kwedwick@users.noreply.github.com>
2021-03-01 20:58:18 -0600

    Start of components and homepage, using semantics-ui but may switch

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Nav = () => {
+    return (
+        <div>
+            I'm Nav
+        </div>
+    )
+}
+
+export default Nav;
---
+++ src/pages/Homepage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Nav from '../components/Nav/Nav'
+import Footer from '../components/Footer/Footer'
+
+const Homepage = () => {
+    return (
+        <div>
+            <Nav></Nav>
+            Hello!
+            <Footer></Footer>
+        </div>
+    )
+}
+
+export default Homepage;
